f68bd46879b280f0695787a1e9d8b6a2aba473cd
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-12 14:39:08 +0300

    create new file also now class comp.js file

---
+++ src/components/Functional_comp.js
@@ -0,0 +1,6 @@
+// import React from 'react';
+// function Hello(){
+//     return <h1>Function components</h1>
+// }
+// export default Hello;
+export const Hello =()=> <h1>Function components</h1>
