29e3a227554af4712bd39f44cfdbd38d618e7bd7
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-12 16:29:41 +0300

    fine work effectively probes work or properties

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Hi from './components/Class_comp';
 import {Hello} from './components/Functional_comp';
 import Wellcome from './components/jsx';
+import Fullname from './components/props';
 function App() {
   return (
     <div className="App">
        {/* <Hello/> */}
       {/* <Hi/> */}
       {/* <Wellcome/> */}
+      <Fullname name="kassaye" Age="20"/>
+      <Fullname name="muse"Age="29"/>
+      <Fullname name="aron"Age="24"/>
     </div>
   );
 }
---
+++ src/components/props.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Fullname = (props) => {
+    return ( <h1>{props.name} siyum {props.Age}</h1> );
+}
+ 
+export default Fullname ;
