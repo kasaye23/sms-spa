557e3fc3872222e3bac4c7181a99cb4fad4fb108
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-12 15:06:30 +0300

    display two component first is function component and second is class component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Hello} from './components/Functional_comp';
 function App() {
   return (
     <div className="App">
-      {/* <Hello/> */}
+       <Hello/>
       <Hi/>
     </div>
   );
--- src/components/Functional_comp.js
+++ src/components/Functional_comp.js
@@ -1,6 +1,15 @@
-// import React from 'react';
-// function Hello(){
-//     return <h1>Function components</h1>
-// }
+ import React from 'react';
+ export class Hello extends React.Component {
+     constructor(props) {
+         super(props);
+     }
+
+     render() {
+         return <h1>Function components</h1>
+     }
+ }
+
+
+
 // export default Hello;
-export const Hello =()=> <h1>Function components</h1>
+//export const Hello =()=> <h1>Function components</h1>
