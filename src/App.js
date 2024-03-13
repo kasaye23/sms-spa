0e3cb9c01aef3fda257f26f40b2484724135c068
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-13 22:41:08 +0300

    now add class in above change file work other way

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ function App() {
        {/* <Hello/> */}
       {/* <Hi/> */}
       {/* <Wellcome/> */}
-      
+      <Fullname name="kassaye" Age="20">
+        <p>This is my react world all are fine work amazing progress today</p>
+         <button>Click me </button>
+         <p>The easiest way to debug websites built with React is to install the React Developer Tools browser extension. It is available for several popular browsers:</p>
+         </Fullname>
+      <Fullname name="muse"Age="29">
+        <h6>wow kassaye </h6>
+      </Fullname>
+      <Fullname name="aron"Age="24"/>
     </div>
   );
 }
