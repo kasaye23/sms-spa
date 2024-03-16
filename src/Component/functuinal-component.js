400c3995c6dc37d277fe625fab9ad3129fc8283e
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-16 14:48:02 +0300

    undo class to function

--- src/Component/functuinal-component.js
+++ src/Component/functuinal-component.js
@@ -1,14 +1,8 @@
  import React from 'react';
 // also this below export is add or re,ove fine work freely but use only in app.js {Kello} this parantecise 
-export class Kello extends React.Component {
- constructor(props) {
-  super(props);
- }
-
- render() {
-      return <h1>Functional component</h1>
-    
- }
+export function Kello(){
+     return <h1>Functional component</h1>
+   
 }
 // export default Kello;
 
