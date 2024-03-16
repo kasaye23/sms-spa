d80ac5e8f15c58f67fc081853e33d86ebca8cb61
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-16 16:19:25 +0300

    add extention and use rcc short cut

--- src/Component/props.js
+++ src/Component/props.js
@@ -1,14 +1,29 @@
-import React from 'react';
-const Full_name = (props) => {
-    return ( 
-        <div>
-        <h2> 
-           {props.name} tekle {props.age} 
-        </h2> 
-        {props.children}
+// import React from 'react';
+// const Full_name = (props) => {
+//     return ( 
+//         <div>
+//         <h2> 
+//            {props.name} tekle {props.age} 
+//         </h2> 
+//         {props.children}
         
-        </div>
-        );
-}
+//         </div>
+//         );
+// }
  
-export default Full_name ;
+// export default Full_name ;
+
+
+
+import React , { Component} from "react";
+export default class props extends Component {
+    render(){
+        return(
+            <div>
+                <h1> {this.props.name}  tekle {this.props.age}</h1>
+            {this.props.children}
+            </div>
+        )
+    }
+}
+
