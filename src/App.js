c6979ec2fc8390dc8902a67a2ba190e5fc51d040
kasaye23 <Kassayeseyoum0@gmail.com>
2024-03-16 13:17:41 +0300

    practice react app saturday 1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Hi from './components/Class_comp';
 import {Hello} from './components/Functional_comp';
 import Wellcome from './components/jsx';
 import Fullname from './components/props';
+import Kello from './Component/functuinal-component';
 function App() {
   return (
     <div className="App">
        {/* <Hello/> */}
       {/* <Hi/> */}
       {/* <Wellcome/> */}
-      <Fullname name="kassaye" Age="20">
+      {/* <Fullname name="kassaye" Age="20">
         <p>This is my react world all are fine work amazing progress today</p>
          <button>Click me </button>
          <p>The easiest way to debug websites built with React is to install the React Developer Tools browser extension. It is available for several popular browsers:</p>
@@ -18,7 +19,8 @@ function App() {
       <Fullname name="muse"Age="29">
         <h6>wow kassaye </h6>
       </Fullname>
-      <Fullname name="aron"Age="24"/>
+      <Fullname name="aron"Age="24"/> */}
+      <Kello></Kello>
     </div>
   );
 }
---
+++ src/Component/functuinal-component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Kello(){
+    return <h1>functional component</h1>
+}
+export default Kello;
